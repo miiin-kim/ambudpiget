940f4b66dc1e303c16f90b3052d3ce694f0e0245
miiin-kim <miiinkim58@gmail.com>
2026-05-14 14:22:27 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE='jaejeong-v13';
+const FILES=['./','./index.html'];
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
